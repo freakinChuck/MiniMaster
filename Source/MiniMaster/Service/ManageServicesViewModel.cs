a068468b113c94b3c3090e3723f0c05c5de9445d
tastappu <silvio.stappung@stud.hslu.ch>
2017-10-05 19:44:17 +0200

    AcolyteDisplay in Service

--- Source/MiniMaster/Service/ManageServicesViewModel.cs
+++ Source/MiniMaster/Service/ManageServicesViewModel.cs
@@ -29,6 +29,43 @@ namespace MiniMaster.Service
             get; set;
         }
 
+        public List<object> JobsWithAssignments
+        {
+            get
+            {
+                var data = Workspace.CurrentData.ServiceJobs.Where(x => x.ServiceId == SelectedService.Id)
+                    .Select(x =>
+                    new
+                    {
+                        JobId = x.JobId,
+                        JobName = Workspace.CurrentData.Jobs.FirstOrDefault(j => j.Id == x.JobId)?.Text ?? "unbekannte Aufgabe",
+                        JobOrder = Workspace.CurrentData.Jobs.FirstOrDefault(j => j.Id == x.JobId)?.Order ?? int.MaxValue,
+                        ServiceId = x.ServiceId,
+                        AcolyteId = x.AcolyteId,
+                        AcolyteName = Workspace.CurrentData.Acolytes.FirstOrDefault(a => a.Id == x.AcolyteId)?.Name,
+                        AcolyteFirstname = Workspace.CurrentData.Acolytes.FirstOrDefault(a => a.Id == x.AcolyteId)?.Firstname,
+                    })
+                    .OrderBy(x => x.JobOrder)
+                    .ThenBy(x => string.IsNullOrEmpty(x.AcolyteId));
+                return
+                    (data.Select(x => 
+                            (object)new
+                            {
+                                Text = string.Format(
+                                                "{0}: {1}", 
+                                                x.JobName, 
+                                                string.IsNullOrEmpty(x.AcolyteId) ? 
+                                                    "keine Zuteilung" : 
+                                                    (string.IsNullOrEmpty(x.AcolyteFirstname) ? 
+                                                        "unbekannter Ministrant" : 
+                                                        string.Format("{0} {1}", x.AcolyteFirstname, x.AcolyteName)
+                                                    )
+                                        )
+                            }).ToList()
+                    );
+            }
+        }
+
         private void LoadJobs()
         {
             var template = this.SelectedService;
@@ -39,8 +76,18 @@ namespace MiniMaster.Service
             else
             {
                 this.AllJobs = new BindingList<ServiceJobViewModel>(Workspace.CurrentData.Jobs.OrderBy(x => x.Order).Select(x => new ServiceJobViewModel(template.storageService, x)).ToList());
+                foreach (var job in AllJobs)
+                {
+                    job.PropertyChanged += Job_PropertyChanged;
+                }
             }
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AllJobs)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(JobsWithAssignments)));
+        }
+
+        private void Job_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(JobsWithAssignments)));
         }
 
         public ManageServiceViewModel()
